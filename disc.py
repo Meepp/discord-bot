395a389d2b9fac2c1c2c2458ee6e88ac758df928
Fjf <fjf@hotmail.nl>
2017-12-29 16:07:20 +0100

    started on music queue
    not done yet pls fix

--- disc.py
+++ disc.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 from random import randint
+import queue
+
 
 client = discord.Client()
 # bot_prefix = "!bot "
@@ -30,6 +32,34 @@ async def command_fuckoff(message):
         if x.server == message.server:
             return await x.disconnect()
 
+class MusicPlayer:
+    def __init__(self):
+        self.queue = queue.Queue()
+        self.player = None
+
+    def play(self):
+        # If player is none and the queue is empty.
+        if not self.player and self.queue.empty():
+            pass
+
+        # Player is none or player is done you may overwrite the player
+        if not self.player or self.player.is_done():
+            if self.player:
+                self.player.stop()
+                print(self.player.error)
+            self.player = self.queue.get();
+            self.player.start()
+
+musicplayer = MusicPlayer()
+
+def add_queue(player):
+    musicplayer.queue.put(player)
+    # stall thread
+    while musicplayer.player and musicplayer.player.is_playing():
+        pass # do nothing
+    musicplayer.play()
+
+# Adds a song to a queue
 async def command_music(message, args):
     if len(args) < 1:
         return
@@ -38,14 +68,17 @@ async def command_music(message, args):
         if voice.server == message.server:
             player = await voice.create_ytdl_player(args[0])
             player.volume = 0.2
-            player.start()
+            add_queue(player)
 
 async def command_joinpub(message):
     for role in message.server.roles:
-        print(role.name)
         if role.name == "@Pubmannen":
-            print(role.name)
-            client.add_roles(message.author, [role])
+            await client.add_roles(message.author, role)
+
+async def command_leavepub(message):
+    for role in message.server.roles:
+        if role.name == "@Pubmannen":
+            await client.remove_roles(message.author, role)
 
 @client.event
 async def on_message(message):
@@ -70,8 +103,13 @@ async def on_message(message):
 
     elif cmd == "!music":
         await command_music(message, args)
+
     elif cmd == "!joinpub":
         await command_joinpub(message)
+
+    elif cmd == "!leavepub":
+        await command_leavepub(message)
+
 
 with open('key', 'r') as f:
     client.run(f.readline().strip())
